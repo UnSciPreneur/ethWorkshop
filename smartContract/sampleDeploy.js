190771924a9a57a2ecdbc05eadb1f0c92e88df50
chansonm <chansonm@ethz.ch>
2017-08-15 13:16:08 +0200

    Created the sample files for a Voting Smart Contract

---
+++ smartContract/sampleDeploy.js
@@ -0,0 +1,8 @@
+var Voting = artifacts.require("./Voting.sol");
+module.exports = function(deployer) {
+  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 290000});
+};
+/* As you can see above, the deployer expects the first argument to   be the name of the contract followed by constructor arguments. In our case, there is only one argument which is an array of
+candidates. The third argument is a hash where we specify the gas required to deploy our code. The gas amount varies depending on the size of your contract. For the Voting contract, 290000 was
+sufficient.
+*/
